4d9cdf10de98d0cc6069bf1de1f4af2297ef89c9
Oleksandr Mekhovov <oleksandr.mekhovov@coupa.com>
2026-09-13 15:17:23 +0200

    chore: prepare v0.34.0 candidate metadata

--- game/app.mjs
+++ game/app.mjs
@@ -195,7 +195,7 @@ try {
     executionCatalog = content.executions;
     masteryCatalog = content.registrations;
   }
-  let buildVersion = '0.33.0',
+  let buildVersion = '0.34.0',
     isRelease = false;
   try {
     buildVersion = (await getJSON('build-info.json')).version;
