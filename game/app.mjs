c94e2ddb7c66fd460261e9a5fd8b20cbfa4f99f7
Oleksandr Mekhovov <oleksandr.mekhovov@coupa.com>
2026-09-14 10:25:03 +0200

    chore: prepare v040 native interface release

--- game/app.mjs
+++ game/app.mjs
@@ -218,7 +218,7 @@ try {
     executionCatalog = content.executions;
     masteryCatalog = content.registrations;
   }
-  let buildVersion = '0.39.0',
+  let buildVersion = '0.40.0',
     isRelease = false;
   try {
     buildVersion = (await getJSON('build-info.json')).version;
